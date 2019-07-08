0c02785f705fba2483d78422ad67a7cb3d8d2386
Patrick Podest <patrick@podest.co.at>
2019-07-08 12:11:05 +0200

    Immediate commit

--- bcfplugin/gui/design/plugin.py
+++ bcfplugin/gui/design/plugin.py
@@ -1,15 +1,19 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QFile
 from ui_plugin import Ui_Form
 
-class MainWindow(QMainWindow, Ui_Form):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setupUi(self)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     plugin = MainWindow()
     plugin.show()
-    app.exec_()
+
+    sys.exit(app.exec_())
